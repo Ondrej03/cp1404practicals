8bb370597cb6ea694c43f1cbaf6c9b35595b360a
Ondrej <536477@muni.cz>
2024-09-23 18:21:23 +0800

    menu

---
+++ prac_01/menu.py
@@ -0,0 +1,24 @@
+MENU = """(H)ello
+(G)oodbye
+(Q)uit"""
+
+name = input("Enter name:")
+print(MENU)
+
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "H":
+        print("Hello " + name)
+    elif choice == "G":
+        print("Goodbye " + name)
+    elif choice == "Q":
+        break
+    else:
+        print("Invalid choice")
+
+    print(MENU)
+    choice = input(">>> ").upper()
+
+print("Finished.")
+
+
