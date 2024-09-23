940e94d0e6d92edf4e8608380c9c5b2309240de8
Ondrej <536477@muni.cz>
2024-09-23 18:06:51 +0800

    loops

---
+++ prac_01/loops.py
@@ -0,0 +1,27 @@
+
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+#a
+for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+
+#b
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+#c
+num_of_stars = int(input("Enter desired number of stars : "))
+for i in range(0, num_of_stars):
+    print("*", end='')
+print()
+
+#d
+for i in range(1, num_of_stars+1):
+    print("*" * i)
+
+
+
