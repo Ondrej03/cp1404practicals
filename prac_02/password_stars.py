54b812036b2baefc7e9686d0f18bf882ae03941a
Ondrej <536477@muni.cz>
2024-09-28 16:59:47 +0800

    style: dividing code into functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,21 @@
 MIN_PASS_LEN = 4
 
-password = input("Type password: ")
-while len(password) < MIN_PASS_LEN:
-    print(f"Password must contain at least {MIN_PASS_LEN} characters")
+def main():
+    password = get_password()
+
+    print_password(password)
+
+
+def print_password(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Type password: ")
+    while len(password) < MIN_PASS_LEN:
+        print(f"Password must contain at least {MIN_PASS_LEN} characters")
+        password = input("Type password: ")
+    return password
 
-print("*" * len(password))
 
+main()
