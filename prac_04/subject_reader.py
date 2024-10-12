d552d97ece64c1361e46ced5cfcf84bd44363b17
Ondrej <536477@muni.cz>
2024-10-12 19:46:57 +0800

    feat: solved task

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
-
+    print_data(data)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +22,14 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subject_data.append(parts)
         print("----------")
     input_file.close()
+    return subject_data
+
+def print_data(data):
+    for parts in data:
+        print(f"{parts[0]} is taught by {parts[1]} and has {str(parts[2])} students")
 
 
 main()
