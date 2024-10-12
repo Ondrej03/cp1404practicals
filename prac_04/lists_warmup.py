cd3483532865bda4ea9642f8ce2c90b1f8f7d024
Ondrej <536477@muni.cz>
2024-10-12 19:14:14 +0800

    feat: solved task

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# a) 3
+# b) 2
+# c) 1
+# d) [3, 1, 4, 1, 5, 9 ]
+# e) [1]
+# f) true
+# g) false
+# h) false
+# i) [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# all correct
+
+# 1
+numbers[0] = "ten"
+
+# 2
+numbers[-1] = 1
+
+# 3
+print(numbers[2:])
+
+# 4
+print(9 in numbers)
