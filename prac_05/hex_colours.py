58acd8622de882d2fe08783ec6a2461c38051fdc
Ondrej <536477@muni.cz>
2024-10-16 12:30:12 +0800

    feat: add colours dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+COLOURS = {"Celadon": "#ace1af", "Cerulean": "#007ba7", "Champagne": "#f7e7ce",
+          "Carmine": "#960018", "Cardinal": "#c41e3a", "Canary": "#ffff99",
+          "Camel": "#c19a6b", "Byzantine": "#bd33a4","Bone": "#e3dac9",
+          "Beige": "#f5f5dc"}
+
