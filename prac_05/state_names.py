1ee888f963c0ce83a4109da8011216a96ab1dc13
Ondrej <536477@muni.cz>
2024-10-16 12:35:14 +0800

    fix: repeated try with lower case working

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -20,4 +20,4 @@ while state_code != "":
         print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
