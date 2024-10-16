a17a89ab90ebe3c65c451ba0842dc9ea15871156
Ondrej <536477@muni.cz>
2024-10-16 12:36:16 +0800

    feat: complete hex_colours.py exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,5 +1,12 @@
-COLOURS = {"Celadon": "#ace1af", "Cerulean": "#007ba7", "Champagne": "#f7e7ce",
-          "Carmine": "#960018", "Cardinal": "#c41e3a", "Canary": "#ffff99",
-          "Camel": "#c19a6b", "Byzantine": "#bd33a4","Bone": "#e3dac9",
-          "Beige": "#f5f5dc"}
+COLOURS = {"CELADON": "#ace1af", "CERULEAN": "#007ba7", "CHAMPAGNE": "#f7e7ce",
+          "CARMINE": "#960018", "CARDINAL": "#c41e3a", "CANARY": "#ffff99",
+          "CAMEL": "#c19a6b", "BYZANTINE": "#bd33a4","BONE": "#e3dac9",
+          "BEIGE": "#f5f5dc"}
 
+color = input("Enter color name: ").upper()
+while color != "":
+    try:
+        print(color, "code is", COLOURS[color])
+    except KeyError:
+        print("Invalid color name")
+    color = input("Enter color name: ").upper()
