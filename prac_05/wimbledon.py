8341cc4197c5d3796299375777abb7f249d001aa
Ondrej <536477@muni.cz>
2024-10-16 21:21:06 +0800

    feat: finish the wimbledon.py task

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,6 +1,4 @@
 def main():
-
-
     champions = []
     countries = set()
     read_data(champions, countries)
@@ -12,22 +10,24 @@ def main():
         else :
             champion_win_count[champion] = 1
 
-
-
-
-
-
-    print(champions)
-    print(countries)
-
+    print_data(champion_win_count, countries)
 
 def read_data(champions, countries):
 
     with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+        next(in_file)
         for row in in_file:
             row_elements = row.split(",")
             champions.append(row_elements[2])
             countries.add(row_elements[1])
 
+def print_data(champion_win_count, countries):
+    print("Wimbledon Champions: ")
+    for champion, win_count in champion_win_count.items():
+        print(f"{champion} {win_count}")
+
+    print()
+    print("These 12 countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
 
 main()
