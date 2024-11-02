7e0d54b825f509fc9a975840de50ffb9802d00c2
Ondrej <536477@muni.cz>
2024-11-03 01:04:35 +0800

    feat: implement guitar exercise

---
+++ prac_06/guitars.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical - Programming languages exercise
+Estimated time : 20 mins
+Actual time spent : 12:12
+"""
+
+from guitar import Guitar
+
+def main():
+    print("My guitars!")
+    my_guitars = []
+
+    while (name := input("Name: ")) != "":
+
+        while not (year := input("Year: ")).isdigit():
+            print("Enter a valid number")
+
+        while not (cost := input("Cost: ")).isdigit():
+            print("Enter a valid number")
+
+        new_guitar = Guitar(name, int(year), int(cost))
+        my_guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+
+
+    print("These are my guitars: ")
+    for index, guitar in enumerate(my_guitars):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {index + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+
+
+
+
+
+
+main()
