92c43d59fe7f7d63b1fca87a6acec0b57d6bc1ab
Ondrej <536477@muni.cz>
2024-11-08 18:34:40 +0800

    feat: reading and printing of guitars file

---
+++ prac_07/guitars_reader.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 Practical
+File and class example - opens/reads a file, stores in objects of custom class
+"""
+
+from guitar import Guitar
+
+
+def main():
+    """Read file of programming language details, save as objects, display."""
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], parts[1], parts[2])
+        guitars.append(guitar)
+
+    in_file.close()
+
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
+
+
