bfaacdfb520ca4ea6b2d1f4097abf897d8819f99
Ondrej <536477@muni.cz>
2024-11-08 19:26:40 +0800

    feat: add project management framework

---
+++ prac_07/project_management.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical - Guitar exercise
+Estimated time : 30 min
+Actual time : 7:24
+"""
+
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
+
+GREETING = "Welcome to Pythonic Project Management"
+FAREWELL = "Thank you for using custom-built project management software."
+
+def main():
+
+    print(GREETING)
+
+    print(FAREWELL)
+
+
+main()
