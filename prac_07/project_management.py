31d861228fc555899fb62acc9835cd51874289b2
Ondrej <536477@muni.cz>
2024-11-08 19:29:54 +0800

    feat: add project class

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,18 @@ Estimated time : 30 min
 Actual time : 7:24
 """
 
+class Project:
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+
+
+
 MENU = """- (L)oad projects  
 - (S)ave projects  
 - (D)isplay projects  
@@ -18,6 +30,8 @@ FAREWELL = "Thank you for using custom-built project management software."
 def main():
 
     print(GREETING)
+
+
 
     print(FAREWELL)
 
