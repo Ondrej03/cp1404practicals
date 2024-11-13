5469f037b32f94dc13e87798fc8ff12a75459872
Ondrej <536477@muni.cz>
2024-11-13 22:51:04 +0800

    feat: complete task 1

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,3 +1,7 @@
+"""
+CP1404/CP5632 Practical - Task 1 - Box Layout
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +11,13 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
